590861eac339c28e360a9cff399e2ccfca8dd066
Patrick Schiefer <patrick.schiefer@cosmoconsult.com>
2023-11-01 16:43:26 +0100

    fix copyright

--- AzureRelay/WinService/BCAgent/Plugins/Fingerprint/Fingerprint.cs
+++ AzureRelay/WinService/BCAgent/Plugins/Fingerprint/Fingerprint.cs
@@ -1,9 +1,4 @@
-﻿// ------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved. 
-// Licensed under the MIT License. See License.txt in the project root for license information. 
-// ------------------------------------------------------------------------------------------------
-
-namespace FingerprintPlugin
+﻿namespace FingerprintPlugin
 {
     using Microsoft.Dynamics.BusinessCentral.Agent.Common;
     using System;
